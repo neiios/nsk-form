0481e6c56a0639654869cfc5361d74ae502ee20f
Igor <68248740+richard96292@users.noreply.github.com>
2022-11-24 16:55:52 +0200

    Make form dynamic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("contact-form");
 const familyStatus = document.getElementById("family-status");
+
 // data from a form
 const data = {};
 
@@ -50,10 +51,10 @@ function changeKids(button) {
     div.classList.add("kid");
     div.innerHTML = `
           <label for="${div.id}-name">Vaiko vardas</label>
-          <input type="text" id="${div.id}-name" />
+          <input type="text" id="${div.id}-name" required />
           <label for="${div.id}-lastname">Vaiko pavardė</label>
-          <input type="text" id="${div.id}-lastname" />
-          <button id="kill-kid" type="button" onclick="changeKids(this)">Ištrinti vaiką</button>
+          <input type="text" id="${div.id}-lastname" required />
+          <button id="kill-kid" type="button" onclick="changeKids(this)">Ištrinti</button>
 `;
     container.appendChild(div);
   } else if (button.id === "kill-kid") {
@@ -62,11 +63,42 @@ function changeKids(button) {
   }
 }
 
+function changeEducation() {
+  if (document.getElementById("education-additional-container")) {
+    document.getElementById("education-additional-container").remove();
+  }
+
+  let container = document.getElementById("education");
+  let select = document.getElementById("education-level");
+
+  if (
+    select.value === "aukstasis-kolegijinis" ||
+    select.value === "aukstasis-universitetinis"
+  ) {
+    let div = document.createElement("div");
+    div.id = "education-additional-container";
+    div.innerHTML = `
+          <label for="qualifications">Kvalifikacija / Laipsnis (išvardyti visus)*</label>
+          <input name="qualifications" required id="qualifications" placeholder="Magistras"></input>
+
+          <label for="degree">Mokslo laipsnis</label>
+          <input type="text" id="degree" placeholder="Daktaras" />
+  `;
+    container.appendChild(div);
+  }
+}
+
 function changeWorkStatus() {
+  if (document.getElementById("study-container")) {
+    document.getElementById("study-container").remove();
+  }
+  // parental leave matches this as well
   if (document.getElementById("works-container")) {
     document.getElementById("works-container").remove();
   }
-  // add 3 more ifs here
+  if (document.getElementById("not-working-container")) {
+    document.getElementById("not-working-container").remove();
+  }
 
   let container = document.getElementById("work-status-container");
   let select = document.getElementById("work-status");
@@ -89,15 +121,52 @@ function changeWorkStatus() {
           <input type="text" id="study-institution" />
   `;
     container.appendChild(div);
-  } else if (select.value === "works") {
+  } else if (select.value === "works" || select.value === "parental-leave") {
     let div = document.createElement("div");
     div.id = "works-container";
-    div.innerHTML = `
-          <label for="work-institution">Darbo įstaiga</label>
-          <input type="text" id="work-institution" />
+    if (select.value === "parental-leave") {
+      div.innerHTML = `
+          <label for="parental-leave-start-date">Atostogų pradžia</label>
+          <input type="date" id="parental-leave-start-date" />
+  `;
+    }
+    div.innerHTML += `
+          <label for="work-institution">Darbo įstaiga*</label>
+          <input type="text" id="work-institution" required />
 
-          <label for="work-functions">Darbo pareigos</label>
-          <input type="text" id="work-functions" />
+          <label for="work-functions">Darbo pareigos*</label>
+          <input type="text" id="work-functions" required/>
+
+          <label for="years-of-experience">Darbo patirtis (metais)*</label>
+          <input type="number" id="years-of-experience" required/>
+
+          <label for="nature-of-work">Darbo pobūdis*</label>
+          <select name="nature-of-work" id="nature-of-work" required>
+            <option value="executive">Vadovas</option>
+            <option value="specialist">Specialistas</option>
+            <option value="self-employed">Individuali veikla</option>
+          </select>
+
+          <label for="work-field">Darbo sritis*</label>
+          <select name="work-field" id="work-field" required>
+            <option value="law">Teisė</option>
+            <option value="public-sector">Viešasis sektorius</option>
+            <option value="health">Sveikatos apsauga</option>
+            <option value="pharmacy">Farmacija</option>
+            <option value="industrial">Pramonė/Gamyba</option>
+            <option value="it">IT</option>
+            <option value="marketing">Prekyba</option>
+            <option value="national-defence">Krašto apsauga</option>
+            <option value="internal-affairs">Vidaus reikalų sistema</option>
+            <option value="customer-service">Klientų aptarnavimas</option>
+            <option value="transport">Transportas</option>
+            <option value="entertainment">Kultūra ir pramogos</option>
+            <option value="education">Švietimas/Studijos</option>
+          </select>
+        </div>
+
+        <label for="work-status-end">Tikėtina profesinės padėties pabaiga*</label>
+        <input type="date" id="work-status-end" required/>
   `;
     container.appendChild(div);
   } else if (select.value === "not-working") {
@@ -108,16 +177,11 @@ function changeWorkStatus() {
           <input type="text" id="not-working-reason" />
   `;
     container.appendChild(div);
-  } else if (select.value === "parental-leave") {
-    let div = document.createElement("div");
-    div.id = "parental-leave-container";
-    div.innerHTML = `
-          <label for="parental-leave-start-date">Atostogų pradžia</label>
-          <input type="date" id="parental-leave-start-date" />
-  `;
-    container.appendChild(div);
   }
 }
 
 form.addEventListener("submit", submissionHandler);
+// run it to change the default value
 changeWorkStatus();
+changeMaritalStatus();
+changeEducation();
